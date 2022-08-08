a0ad804ef71168b7d9a8711d81928dd040a8fdab
Sasha <volfym79@gmail.com>
2022-08-08 16:05:19 +0200

    add autoscroll on mobile. make text bigger. split out name. comment out bg boxes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const btn = document.querySelector(".fzbz button");
 const div = document.querySelector(".fzbz div");
 
 let x = 0;
+let w = window.innerWidth;
 
 btn.addEventListener('click', () => {
     div.classList.remove("fadeon", "fadeon-fz", "fadeon-fz-bz", "fadeon-bz");
@@ -19,4 +20,7 @@ btn.addEventListener('click', () => {
     
     div.textContent = text === "" ? x : text;
     setTimeout(() => div.classList.add(className), 1);
+    if(w<=800){
+        window.scrollTo(0, document.body.scrollHeight);
+    }
 });
